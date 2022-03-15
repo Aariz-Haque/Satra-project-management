2c40e03de42d60d5e70070923b5693054c3521f3
Aariz-Haque <haqaariz2008@gmail.com>
2022-03-15 08:53:49 +0530

    addedDomainname

--- goonjan/settings.py
+++ goonjan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2ospq&$_@l!%*yggjc=ee54=ov+nd%frv9!)v#4!v(ouknq-az
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','satraprojectmanagement.herokuapp.com']
 
 
 # Application definition
