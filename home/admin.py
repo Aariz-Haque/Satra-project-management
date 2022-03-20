95e05a1ec41d58048e6a8243344feed65323206c
Aariz-Haque <haqaariz2008@gmail.com>
2022-03-20 17:00:37 +0530

    asdf

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Beneficiary,Patient,Report
+from .models import Beneficiary,Patient,Report,Camp
 # Register your models here.
 admin.site.register(Beneficiary)
 admin.site.register(Patient)
 admin.site.register(Report)
+admin.site.register(Camp)
