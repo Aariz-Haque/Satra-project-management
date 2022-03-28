5fc3197f2ba1e6e97bb18bf11917242c874d249f
Aariz-Haque <haqaariz2008@gmail.com>
2022-03-28 15:42:10 +0530

    final fixes

--- home/models.py
+++ home/models.py
@@ -256,6 +256,7 @@ class Camp(models.Model):
         db_table = 'Patient Camp'
 
 class ScreeningCamp(models.Model):
+    id=models.AutoField(primary_key=True)
     beneficiary=models.ForeignKey(Beneficiary,on_delete=models.CASCADE,null=True,blank=True)
     name=models.CharField(max_length=200,blank=True,null=True)
     dob=models.DateField(blank=True,null=True)
@@ -292,4 +293,4 @@ class ScreeningCamp(models.Model):
     prescription=models.TextField(blank=True,null=True)
     date_of_camp=models.DateField(blank=True,null=True)
     def __str__(self):
-        return self.name
+        return str(self.id)+' '+self.name+' '+str(self.date_of_camp)
