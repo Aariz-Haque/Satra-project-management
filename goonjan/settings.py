bdc6dd32420167f2e79f39beeae642a53d8e26cb
Aariz-Haque <87598569+Aariz-Haque@users.noreply.github.com>
2022-03-30 13:10:52 +0530

    Update settings.py

--- goonjan/settings.py
+++ goonjan/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2ospq&$_@l!%*yggjc=ee54=ov+nd%frv9!)v#4!v(ouknq-az'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','satraprojectmanagement.herokuapp.com']
 
